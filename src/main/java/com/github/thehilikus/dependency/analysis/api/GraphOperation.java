6592a9b8df88a03766551c06187e10a6056e7ddf
Alejandro Endo <theHilikus@gmail.com>
2015-03-04 22:22:08 -0500

    Started defining GraphOperation interface
    Added a long description and an execute method that is invoked by the
    Graph implementations

--- src/main/java/com/github/thehilikus/dependency/analysis/api/GraphOperation.java
+++ src/main/java/com/github/thehilikus/dependency/analysis/api/GraphOperation.java
@@ -1,5 +1,8 @@
 package com.github.thehilikus.dependency.analysis.api;
 
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
 /**
  * A command to apply to a graph
  *
@@ -8,8 +11,23 @@ package com.github.thehilikus.dependency.analysis.api;
 public interface GraphOperation {
 
     /**
-     * @return a unique identifier of the operation
+     * @return the short name of the operation
      */
     String getName();
+
+    /**
+     * @return a longer definition of what the operation does
+     */
+    String getDescription();
+
+    /**
+     * Executes the operation on the given graph. This method is intended to be invoked by the Graph
+     * implementation
+     * 
+     * @param sourceName the name of the graph on which the operation is acting
+     * @param graph the internal graph to act on
+     * @return a resulting graph after applying the operation
+     */
+    Graph execute(String sourceName, DirectedGraph<String, DefaultEdge> graph);
 
 }
--- src/main/java/com/github/thehilikus/dependency/analysis/sessions/GraphOperationSession.java
+++ src/main/java/com/github/thehilikus/dependency/analysis/sessions/GraphOperationSession.java
@@ -25,7 +25,6 @@ public class GraphOperationSession implements Runnable {
     @Override
     public void run() {
 	graph.apply(command);
-
     }
 
 }
