b3af29e630ddd63dff10f28e87f50e8bde8588b7
Alejandro Endo <theHilikus@gmail.com>
2015-03-08 01:20:00 -0500

    Added logic to create a graph creation session

--- src/main/java/com/github/thehilikus/dependency/analysis/gui/controller/MainController.java
+++ src/main/java/com/github/thehilikus/dependency/analysis/gui/controller/MainController.java
@@ -1,11 +1,25 @@
 package com.github.thehilikus.dependency.analysis.gui.controller;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javafx.fxml.FXML;
 import javafx.stage.DirectoryChooser;
 
+import com.github.thehilikus.dependency.analysis.api.DependencySource;
+import com.github.thehilikus.dependency.analysis.api.Graph;
+import com.github.thehilikus.dependency.analysis.core.PackageGraphCreator;
 import com.github.thehilikus.dependency.analysis.gui.MainPanel;
+import com.github.thehilikus.dependency.analysis.readers.java.JavaSourceCodeReader;
+import com.github.thehilikus.dependency.analysis.sessions.GraphCreationSession;
 
 /**
  * A controller of user events of MainPanel
@@ -15,25 +29,53 @@ import com.github.thehilikus.dependency.analysis.gui.MainPanel;
 public class MainController {
     private MainPanel mainPanel;
 
+    private Set<DependencySource> dependencySources = new HashSet<>();
+
+    private ExecutorService executor;
+
+    private Future<Graph> operation;
+
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
     @FXML
     private void initialize() {
-
+	executor = Executors.newSingleThreadExecutor();
     }
 
     @FXML
     private void createPackageGraphSession() {
-
+	if (dependencySources.isEmpty()) {
+	    log.error("[createPackageGraphSession] Please set a dependency provider first");
+	    // TODO: add Alert dialog when openjfx 8u40 is released
+	    return;
+	}
+	PackageGraphCreator packageGraph = new PackageGraphCreator();
+	GraphCreationSession graphSession = new GraphCreationSession("test", packageGraph, dependencySources);
+	
+	if (!operation.isDone()) {
+	    log.info("[createPackageGraphSession] Previous task was not finished. Cancelling it");
+	    operation.cancel(true);
+	}
+	
+	operation = executor.submit(graphSession);
     }
 
     @FXML
-    private void selectSourceCodeFolder() {
+    private void selectJavaSourceCodeFolder() {
+	dependencySources.clear();
+
 	DirectoryChooser folderChooser = new DirectoryChooser();
 	folderChooser.setTitle("Select root folder to scan for java source code");
 	File rootFolder = folderChooser.showDialog(mainPanel.getPrimaryStage());
 	if (rootFolder != null) {
-
+	    try {
+		JavaSourceCodeReader sourceCodeReader = new JavaSourceCodeReader(rootFolder.toPath());
+		dependencySources.add(sourceCodeReader);
+	    } catch (IOException exc) {
+		log.error("[selectJavaSourceCodeFolder] There was a problem reading source code: ", exc);
+		// TODO: add Alert dialog when openjfx 8u40 is released
+	    }
 	}
-
     }
 
     /**
