4d7b711b45a758cc13d6d93a26ab6780aea6df00
Alejandro Endo <theHilikus@gmail.com>
2015-03-08 18:54:52 -0400

    Added convenience constructor that takes a single dependency source

--- src/main/java/com/github/thehilikus/dependency/analysis/sessions/GraphCreationSession.java
+++ src/main/java/com/github/thehilikus/dependency/analysis/sessions/GraphCreationSession.java
@@ -1,5 +1,6 @@
 package com.github.thehilikus.dependency.analysis.sessions;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
@@ -19,6 +20,8 @@ public class GraphCreationSession implements Callable<Graph> {
     private final String graphName;
 
     /**
+     * Creates a session with multiple dependency sources
+     * 
      * @param name the unique name to give to the graph
      * @param creator the object used to construct the graph
      * @param sources the group of dependency sources
@@ -27,6 +30,24 @@ public class GraphCreationSession implements Callable<Graph> {
 	graphName = name;
 	graphCreator = creator;
 	dependenciesSources = sources;
+    }
+
+    /**
+     * Creates a session with a single dependency source
+     * 
+     * @param name the unique name to give to the graph
+     * @param creator the object used to construct the graph
+     * @param source the dependency source
+     */
+    public GraphCreationSession(String name, GraphCreator creator, DependencySource source) {
+	this(name, creator, createSetWithElement(source));
+    }
+
+    private static Set<DependencySource> createSetWithElement(DependencySource source) {
+	Set<DependencySource> result = new HashSet<>();
+	result.add(source);
+
+	return result;
     }
 
     @Override
